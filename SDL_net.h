88116128802252430245280f480b77c92837e0eb
Sam Lantinga <slouken@libsdl.org>
2013-05-21 21:27:39 -0700

    Cleaned up whitespace for the 2.0.0 release

--- SDL_net.h
+++ SDL_net.h
@@ -32,9 +32,9 @@ typedef uint16_t Uint16;
 typedef uint32_t Uint32;
 
 typedef struct SDLNet_version {
-	Uint8 major;
-	Uint8 minor;
-	Uint8 patch;
+    Uint8 major;
+    Uint8 minor;
+    Uint8 patch;
 } SDLNet_version;
 
 #else /* WITHOUT_SDL */
@@ -56,18 +56,18 @@ extern "C" {
 
 /* Printable format: "%d.%d.%d", MAJOR, MINOR, PATCHLEVEL
 */
-#define SDL_NET_MAJOR_VERSION	1
-#define SDL_NET_MINOR_VERSION	2
-#define SDL_NET_PATCHLEVEL	9
+#define SDL_NET_MAJOR_VERSION   2
+#define SDL_NET_MINOR_VERSION   0
+#define SDL_NET_PATCHLEVEL      0
 
 /* This macro can be used to fill a version structure with the compile-time
  * version of the SDL_net library.
  */
-#define SDL_NET_VERSION(X)						\
-{									\
-	(X)->major = SDL_NET_MAJOR_VERSION;				\
-	(X)->minor = SDL_NET_MINOR_VERSION;				\
-	(X)->patch = SDL_NET_PATCHLEVEL;				\
+#define SDL_NET_VERSION(X)                          \
+{                                                   \
+    (X)->major = SDL_NET_MAJOR_VERSION;             \
+    (X)->minor = SDL_NET_MINOR_VERSION;             \
+    (X)->patch = SDL_NET_PATCHLEVEL;                \
 }
 
 /* This function gets the version of the dynamically linked SDL_net library.
@@ -88,8 +88,8 @@ extern DECLSPEC void SDLCALL SDLNet_Quit(void);
 /***********************************************************************/
 
 typedef struct {
-	Uint32 host;			/* 32-bit IPv4 host address */
-	Uint16 port;			/* 16-bit protocol port */
+    Uint32 host;            /* 32-bit IPv4 host address */
+    Uint16 port;            /* 16-bit protocol port */
 } IPaddress;
 
 /* Resolve a host name and port to an IP address in network form.
@@ -99,16 +99,16 @@ typedef struct {
    If 'host' is NULL, the resolved host will be set to INADDR_ANY.
  */
 #ifndef INADDR_ANY
-#define INADDR_ANY		0x00000000
+#define INADDR_ANY      0x00000000
 #endif
 #ifndef INADDR_NONE
-#define INADDR_NONE		0xFFFFFFFF
+#define INADDR_NONE     0xFFFFFFFF
 #endif
 #ifndef INADDR_LOOPBACK
-#define INADDR_LOOPBACK		0x7f000001
+#define INADDR_LOOPBACK     0x7f000001
 #endif
 #ifndef INADDR_BROADCAST
-#define INADDR_BROADCAST	0xFFFFFFFF
+#define INADDR_BROADCAST    0xFFFFFFFF
 #endif
 extern DECLSPEC int SDLCALL SDLNet_ResolveHost(IPaddress *address, const char *host, Uint16 port);
 
@@ -134,7 +134,7 @@ typedef struct _TCPsocket *TCPsocket;
    If ip.host is INADDR_NONE or INADDR_ANY, this creates a local server
    socket on the given port, otherwise a TCP connection to the remote
    host and port is attempted. The address passed in should already be
-   swapped to network byte order (addresses returned from 
+   swapped to network byte order (addresses returned from
    SDLNet_ResolveHost() are already in the correct form).
    The newly created socket is returned, or NULL if there was an error.
 */
@@ -156,7 +156,7 @@ extern DECLSPEC IPaddress * SDLCALL SDLNet_TCP_GetPeerAddress(TCPsocket sock);
    closed, or an unknown socket error occurred.
 */
 extern DECLSPEC int SDLCALL SDLNet_TCP_Send(TCPsocket sock, const void *data,
-		int len);
+        int len);
 
 /* Receive up to 'maxlen' bytes of data over the non-server socket 'sock',
    and store them in the buffer pointed to by 'data'.
@@ -175,18 +175,18 @@ extern DECLSPEC void SDLCALL SDLNet_TCP_Close(TCPsocket sock);
 /***********************************************************************/
 
 /* The maximum channels on a a UDP socket */
-#define SDLNET_MAX_UDPCHANNELS	32
+#define SDLNET_MAX_UDPCHANNELS  32
 /* The maximum addresses bound to a single UDP socket channel */
-#define SDLNET_MAX_UDPADDRESSES	4
+#define SDLNET_MAX_UDPADDRESSES 4
 
 typedef struct _UDPsocket *UDPsocket;
 typedef struct {
-	int channel;		/* The src/dst channel of the packet */
-	Uint8 *data;		/* The packet data */
-	int len;		/* The length of the packet data */
-	int maxlen;		/* The size of the data buffer */
-	int status;		/* packet status after sending */
-	IPaddress address;		/* The source/dest address of an incoming/outgoing packet */
+    int channel;        /* The src/dst channel of the packet */
+    Uint8 *data;        /* The packet data */
+    int len;            /* The length of the packet data */
+    int maxlen;         /* The size of the data buffer */
+    int status;         /* packet status after sending */
+    IPaddress address;  /* The source/dest address of an incoming/outgoing packet */
 } UDPpacket;
 
 /* Allocate/resize/free a single UDP packet 'size' bytes long.
@@ -232,7 +232,7 @@ extern DECLSPEC int SDLCALL SDLNet_UDP_Bind(UDPsocket sock, int channel, const I
 /* Unbind all addresses from the given channel */
 extern DECLSPEC void SDLCALL SDLNet_UDP_Unbind(UDPsocket sock, int channel);
 
-/* Get the primary IP address of the remote system associated with the 
+/* Get the primary IP address of the remote system associated with the
    socket and channel.  If the channel is -1, then the primary IP port
    of the UDP socket is returned -- this is only meaningful for sockets
    opened with a specific port.
@@ -243,7 +243,7 @@ extern DECLSPEC IPaddress * SDLCALL SDLNet_UDP_GetPeerAddress(UDPsocket sock, in
 /* Send a vector of packets to the the channels specified within the packet.
    If the channel specified in the packet is -1, the packet will be sent to
    the address in the 'src' member of the packet.
-   Each packet will be updated with the status of the packet after it has 
+   Each packet will be updated with the status of the packet after it has
    been sent, -1 if the packet send failed.
    This function returns the number of packets sent.
 */
@@ -299,7 +299,7 @@ typedef struct _SDLNet_SocketSet *SDLNet_SocketSet;
 
 /* Any network socket can be safely cast to this socket type */
 typedef struct _SDLNet_GenericSocket {
-	int ready;
+    int ready;
 } *SDLNet_GenericSocket;
 
 /* Allocate a socket set for use with SDLNet_CheckSockets()
@@ -312,11 +312,11 @@ extern DECLSPEC SDLNet_SocketSet SDLCALL SDLNet_AllocSocketSet(int maxsockets);
 extern DECLSPEC int SDLCALL SDLNet_AddSocket(SDLNet_SocketSet set, SDLNet_GenericSocket sock);
 static __inline__ int SDLNet_TCP_AddSocket(SDLNet_SocketSet set, TCPsocket sock)
 {
-	return SDLNet_AddSocket(set, (SDLNet_GenericSocket)sock);
+    return SDLNet_AddSocket(set, (SDLNet_GenericSocket)sock);
 }
 static __inline__ int SDLNet_UDP_AddSocket(SDLNet_SocketSet set, UDPsocket sock)
 {
-	return SDLNet_AddSocket(set, (SDLNet_GenericSocket)sock);
+    return SDLNet_AddSocket(set, (SDLNet_GenericSocket)sock);
 }
 
 
@@ -324,18 +324,18 @@ static __inline__ int SDLNet_UDP_AddSocket(SDLNet_SocketSet set, UDPsocket sock)
 extern DECLSPEC int SDLCALL SDLNet_DelSocket(SDLNet_SocketSet set, SDLNet_GenericSocket sock);
 static __inline__ int SDLNet_TCP_DelSocket(SDLNet_SocketSet set, TCPsocket sock)
 {
-	return SDLNet_DelSocket(set, (SDLNet_GenericSocket)sock);
+    return SDLNet_DelSocket(set, (SDLNet_GenericSocket)sock);
 }
 static __inline__ int SDLNet_UDP_DelSocket(SDLNet_SocketSet set, UDPsocket sock)
 {
-	return SDLNet_DelSocket(set, (SDLNet_GenericSocket)sock);
+    return SDLNet_DelSocket(set, (SDLNet_GenericSocket)sock);
 }
 
 /* This function checks to see if data is available for reading on the
    given set of sockets.  If 'timeout' is 0, it performs a quick poll,
    otherwise the function returns when either data is available for
    reading, or the timeout in milliseconds has elapsed, which ever occurs
-   first.  This function returns the number of sockets ready for reading, 
+   first.  This function returns the number of sockets ready for reading,
    or -1 if there was an error with the select() system call.
 */
 extern DECLSPEC int SDLCALL SDLNet_CheckSockets(SDLNet_SocketSet set, Uint32 timeout);
@@ -347,7 +347,7 @@ extern DECLSPEC int SDLCALL SDLNet_CheckSockets(SDLNet_SocketSet set, Uint32 tim
 #define SDLNet_SocketReady(sock) _SDLNet_SocketReady((SDLNet_GenericSocket)(sock))
 static __inline__ int _SDLNet_SocketReady(SDLNet_GenericSocket sock)
 {
-	return (sock != NULL) && (sock->ready);
+    return (sock != NULL) && (sock->ready);
 }
 
 /* Free a set of sockets allocated by SDL_NetAllocSocketSet() */
@@ -366,10 +366,10 @@ extern DECLSPEC const char * SDLCALL SDLNet_GetError(void);
 
 /* Warning, some systems have data access alignment restrictions */
 #if defined(sparc) || defined(mips) || defined(__arm__)
-#define SDL_DATA_ALIGNED	1
+#define SDL_DATA_ALIGNED    1
 #endif
 #ifndef SDL_DATA_ALIGNED
-#define SDL_DATA_ALIGNED	0
+#define SDL_DATA_ALIGNED    0
 #endif
 
 /* Write a 16/32-bit value to network packet buffer */
@@ -384,52 +384,52 @@ extern DECLSPEC const char * SDLCALL SDLNet_GetError(void);
 
 static __inline__ void _SDLNet_Write16(Uint16 value, void *areap)
 {
-	*(Uint16 *)areap = SDL_SwapBE16(value);
+    *(Uint16 *)areap = SDL_SwapBE16(value);
 }
 
 static __inline__ void _SDLNet_Write32(Uint32 value, void *areap)
 {
-	*(Uint32 *)areap = SDL_SwapBE32(value);
+    *(Uint32 *)areap = SDL_SwapBE32(value);
 }
 
 static __inline__ Uint16 _SDLNet_Read16(void *areap)
 {
-	return SDL_SwapBE16(*(Uint16 *)areap);
+    return SDL_SwapBE16(*(Uint16 *)areap);
 }
 
 static __inline__ Uint32 _SDLNet_Read32(void *areap)
 {
-	return SDL_SwapBE32(*(Uint32 *)areap);
+    return SDL_SwapBE32(*(Uint32 *)areap);
 }
 
 #else /* !defined(WITHOUT_SDL) && !SDL_DATA_ALIGNED */
 
 static __inline__ void _SDLNet_Write16(Uint16 value, void *areap)
 {
-	Uint8 *area = (Uint8*)areap;
-	area[0] = (value >>  8) & 0xFF;
-	area[1] =  value        & 0xFF;
+    Uint8 *area = (Uint8*)areap;
+    area[0] = (value >>  8) & 0xFF;
+    area[1] =  value        & 0xFF;
 }
 
 static __inline__ void _SDLNet_Write32(Uint32 value, void *areap)
 {
-	Uint8 *area = (Uint8*)areap;
-	area[0] = (value >> 24) & 0xFF;
-	area[1] = (value >> 16) & 0xFF;
-	area[2] = (value >>  8) & 0xFF;
-	area[3] =  value        & 0xFF;
+    Uint8 *area = (Uint8*)areap;
+    area[0] = (value >> 24) & 0xFF;
+    area[1] = (value >> 16) & 0xFF;
+    area[2] = (value >>  8) & 0xFF;
+    area[3] =  value        & 0xFF;
 }
 
 static __inline__ Uint16 _SDLNet_Read16(void *areap)
 {
-	Uint8 *area = (Uint8*)areap;
-	return ((Uint16)area[0]) << 8 | ((Uint16)area[1]);
+    Uint8 *area = (Uint8*)areap;
+    return ((Uint16)area[0]) << 8 | ((Uint16)area[1]);
 }
 
 static __inline__ Uint32 _SDLNet_Read32(void *areap)
 {
-	Uint8 *area = (Uint8*)areap;
-	return ((Uint32)area[0]) << 24 | ((Uint32)area[1]) << 16 | ((Uint32)area[2]) << 8 | ((Uint32)area[3]);
+    Uint8 *area = (Uint8*)areap;
+    return ((Uint32)area[0]) << 24 | ((Uint32)area[1]) << 16 | ((Uint32)area[2]) << 8 | ((Uint32)area[3]);
 }
 
 #endif /* !defined(WITHOUT_SDL) && !SDL_DATA_ALIGNED */
--- SDLnet.c
+++ SDLnet.c
@@ -32,9 +32,9 @@
 
 const SDLNet_version *SDLNet_Linked_Version(void)
 {
-	static SDLNet_version linked_version;
-	SDL_NET_VERSION(&linked_version);
-	return(&linked_version);
+    static SDLNet_version linked_version;
+    SDL_NET_VERSION(&linked_version);
+    return(&linked_version);
 }
 
 /* Since the UNIX/Win32/BeOS code is so different from MacOS,
@@ -50,12 +50,12 @@ static int SDLNet_started = 0;
 
 int SDLNet_GetLastError(void)
 {
-	return errno;
+    return errno;
 }
 
 void SDLNet_SetLastError(int err)
 {
-	errno = err;
+    errno = err;
 }
 
 #endif
@@ -64,101 +64,101 @@ static char errorbuf[1024];
 
 void SDLCALL SDLNet_SetError(const char *fmt, ...)
 {
-	va_list argp;
-	va_start(argp, fmt);
-	SDL_vsnprintf(errorbuf, sizeof(errorbuf), fmt, argp);
-	va_end(argp);
+    va_list argp;
+    va_start(argp, fmt);
+    SDL_vsnprintf(errorbuf, sizeof(errorbuf), fmt, argp);
+    va_end(argp);
 #ifndef WITHOUT_SDL
-	SDL_SetError("%s", errorbuf);
+    SDL_SetError("%s", errorbuf);
 #endif
 }
 
 const char * SDLCALL SDLNet_GetError(void)
 {
 #ifdef WITHOUT_SDL
-	return errorbuf;
+    return errorbuf;
 #else
-	return SDL_GetError();
+    return SDL_GetError();
 #endif
 }
 
 /* Initialize/Cleanup the network API */
 int  SDLNet_Init(void)
 {
-	if ( !SDLNet_started ) {
+    if ( !SDLNet_started ) {
 #ifdef __USE_W32_SOCKETS
-		/* Start up the windows networking */
-		WORD version_wanted = MAKEWORD(1,1);
-		WSADATA wsaData;
+        /* Start up the windows networking */
+        WORD version_wanted = MAKEWORD(1,1);
+        WSADATA wsaData;
 
-		if ( WSAStartup(version_wanted, &wsaData) != 0 ) {
-			SDLNet_SetError("Couldn't initialize Winsock 1.1\n");
-			return(-1);
-		}
+        if ( WSAStartup(version_wanted, &wsaData) != 0 ) {
+            SDLNet_SetError("Couldn't initialize Winsock 1.1\n");
+            return(-1);
+        }
 #else
-		/* SIGPIPE is generated when a remote socket is closed */
-		void (*handler)(int);
-		handler = signal(SIGPIPE, SIG_IGN);
-		if ( handler != SIG_DFL ) {
-			signal(SIGPIPE, handler);
-		}
+        /* SIGPIPE is generated when a remote socket is closed */
+        void (*handler)(int);
+        handler = signal(SIGPIPE, SIG_IGN);
+        if ( handler != SIG_DFL ) {
+            signal(SIGPIPE, handler);
+        }
 #endif
-	}
-	++SDLNet_started;
-	return(0);
+    }
+    ++SDLNet_started;
+    return(0);
 }
 void SDLNet_Quit(void)
 {
-	if ( SDLNet_started == 0 ) {
-		return;
-	}
-	if ( --SDLNet_started == 0 ) {
+    if ( SDLNet_started == 0 ) {
+        return;
+    }
+    if ( --SDLNet_started == 0 ) {
 #ifdef __USE_W32_SOCKETS
-		/* Clean up windows networking */
-		if ( WSACleanup() == SOCKET_ERROR ) {
-			if ( WSAGetLastError() == WSAEINPROGRESS ) {
+        /* Clean up windows networking */
+        if ( WSACleanup() == SOCKET_ERROR ) {
+            if ( WSAGetLastError() == WSAEINPROGRESS ) {
 #ifndef _WIN32_WCE
-				WSACancelBlockingCall();
+                WSACancelBlockingCall();
 #endif
-				WSACleanup();
-			}
-		}
+                WSACleanup();
+            }
+        }
 #else
-		/* Restore the SIGPIPE handler */
-		void (*handler)(int);
-		handler = signal(SIGPIPE, SIG_DFL);
-		if ( handler != SIG_IGN ) {
-			signal(SIGPIPE, handler);
-		}
+        /* Restore the SIGPIPE handler */
+        void (*handler)(int);
+        handler = signal(SIGPIPE, SIG_DFL);
+        if ( handler != SIG_IGN ) {
+            signal(SIGPIPE, handler);
+        }
 #endif
-	}
+    }
 }
 
 /* Resolve a host name and port to an IP address in network form */
 int SDLNet_ResolveHost(IPaddress *address, const char *host, Uint16 port)
 {
-	int retval = 0;
+    int retval = 0;
 
-	/* Perform the actual host resolution */
-	if ( host == NULL ) {
-		address->host = INADDR_ANY;
-	} else {
-		address->host = inet_addr(host);
-		if ( address->host == INADDR_NONE ) {
-			struct hostent *hp;
+    /* Perform the actual host resolution */
+    if ( host == NULL ) {
+        address->host = INADDR_ANY;
+    } else {
+        address->host = inet_addr(host);
+        if ( address->host == INADDR_NONE ) {
+            struct hostent *hp;
 
-			hp = gethostbyname(host);
-			if ( hp ) {
-				memcpy(&address->host,hp->h_addr,hp->h_length);
-			} else {
-				retval = -1;
-			}
-		}
-	}
-	address->port = SDLNet_Read16(&port);
+            hp = gethostbyname(host);
+            if ( hp ) {
+                memcpy(&address->host,hp->h_addr,hp->h_length);
+            } else {
+                retval = -1;
+            }
+        }
+    }
+    address->port = SDLNet_Read16(&port);
 
-	/* Return the status */
-	return(retval);
+    /* Return the status */
+    return(retval);
 }
 
 /* Resolve an ip address to a host name in canonical form.
@@ -172,91 +172,91 @@ int SDLNet_ResolveHost(IPaddress *address, const char *host, Uint16 port)
  */
 const char *SDLNet_ResolveIP(const IPaddress *ip)
 {
-	struct hostent *hp;
-	struct in_addr in;
+    struct hostent *hp;
+    struct in_addr in;
 
-	hp = gethostbyaddr((const char *)&ip->host, sizeof(ip->host), AF_INET);
-	if ( hp != NULL ) {
-		return hp->h_name;
-	}
+    hp = gethostbyaddr((const char *)&ip->host, sizeof(ip->host), AF_INET);
+    if ( hp != NULL ) {
+        return hp->h_name;
+    }
 
-	in.s_addr = ip->host;
-	return inet_ntoa(in);
+    in.s_addr = ip->host;
+    return inet_ntoa(in);
 }
 
 int SDLNet_GetLocalAddresses(IPaddress *addresses, int maxcount)
 {
-	int count = 0;
+    int count = 0;
 #ifdef SIOCGIFCONF
 /* Defined on Mac OS X */
 #ifndef _SIZEOF_ADDR_IFREQ
 #define _SIZEOF_ADDR_IFREQ sizeof
 #endif
-	SOCKET sock;
-	struct ifconf conf;
-	char data[4096];
-	struct ifreq *ifr;
-	struct sockaddr_in *sock_addr;
+    SOCKET sock;
+    struct ifconf conf;
+    char data[4096];
+    struct ifreq *ifr;
+    struct sockaddr_in *sock_addr;
 
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if ( sock == INVALID_SOCKET ) {
-		return 0;
-	}
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if ( sock == INVALID_SOCKET ) {
+        return 0;
+    }
 
-	conf.ifc_len = sizeof(data);
-	conf.ifc_buf = (caddr_t) data;
-	if ( ioctl(sock, SIOCGIFCONF, &conf) < 0 ) {
-		closesocket(sock);
-		return 0;
-	}
+    conf.ifc_len = sizeof(data);
+    conf.ifc_buf = (caddr_t) data;
+    if ( ioctl(sock, SIOCGIFCONF, &conf) < 0 ) {
+        closesocket(sock);
+        return 0;
+    }
 
-	ifr = (struct ifreq*)data;
-	while ((char*)ifr < data+conf.ifc_len) {
-		if (ifr->ifr_addr.sa_family == AF_INET) {
-			if (count < maxcount) {
-				sock_addr = (struct sockaddr_in*)&ifr->ifr_addr;
-				addresses[count].host = sock_addr->sin_addr.s_addr;
-				addresses[count].port = sock_addr->sin_port;
-			}
-			++count;
-		}
-		ifr = (struct ifreq*)((char*)ifr + _SIZEOF_ADDR_IFREQ(*ifr));
-	}
-	closesocket(sock);
+    ifr = (struct ifreq*)data;
+    while ((char*)ifr < data+conf.ifc_len) {
+        if (ifr->ifr_addr.sa_family == AF_INET) {
+            if (count < maxcount) {
+                sock_addr = (struct sockaddr_in*)&ifr->ifr_addr;
+                addresses[count].host = sock_addr->sin_addr.s_addr;
+                addresses[count].port = sock_addr->sin_port;
+            }
+            ++count;
+        }
+        ifr = (struct ifreq*)((char*)ifr + _SIZEOF_ADDR_IFREQ(*ifr));
+    }
+    closesocket(sock);
 #elif defined(__WIN32__)
     PIP_ADAPTER_INFO pAdapterInfo;
     PIP_ADAPTER_INFO pAdapter;
-	PIP_ADDR_STRING pAddress;
+    PIP_ADDR_STRING pAddress;
     DWORD dwRetVal = 0;
     ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
 
     pAdapterInfo = (IP_ADAPTER_INFO *) malloc(sizeof (IP_ADAPTER_INFO));
     if (pAdapterInfo == NULL) {
-		return 0;
+        return 0;
     }
 
     if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == ERROR_BUFFER_OVERFLOW) {
         pAdapterInfo = (IP_ADAPTER_INFO *) realloc(pAdapterInfo, ulOutBufLen);
         if (pAdapterInfo == NULL) {
-			return 0;
+            return 0;
         }
-		dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen);
+        dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen);
     }
 
     if (dwRetVal == NO_ERROR) {
         for (pAdapter = pAdapterInfo; pAdapter; pAdapter = pAdapter->Next) {
-			for (pAddress = &pAdapterInfo->IpAddressList; pAddress; pAddress = pAddress->Next) {
-				if (count < maxcount) {
-					addresses[count].host = inet_addr(pAddress->IpAddress.String);
-					addresses[count].port = 0;
-				}
-				++count;
-			}
+            for (pAddress = &pAdapterInfo->IpAddressList; pAddress; pAddress = pAddress->Next) {
+                if (count < maxcount) {
+                    addresses[count].host = inet_addr(pAddress->IpAddress.String);
+                    addresses[count].port = 0;
+                }
+                ++count;
+            }
         }
     }
-	free(pAdapterInfo);
+    free(pAdapterInfo);
 #endif
-	return count;
+    return count;
 }
 
 #if !defined(WITHOUT_SDL) && !SDL_DATA_ALIGNED /* function versions for binary compatibility */
@@ -276,22 +276,22 @@ extern DECLSPEC Uint32 SDLCALL SDLNet_Read32(void *area);
 
 void  SDLNet_Write16(Uint16 value, void *areap)
 {
-	(*(Uint16 *)(areap) = SDL_SwapBE16(value));
+    (*(Uint16 *)(areap) = SDL_SwapBE16(value));
 }
 
 void   SDLNet_Write32(Uint32 value, void *areap)
 {
-	*(Uint32 *)(areap) = SDL_SwapBE32(value);
+    *(Uint32 *)(areap) = SDL_SwapBE32(value);
 }
 
 Uint16 SDLNet_Read16(void *areap)
 {
-	return (SDL_SwapBE16(*(Uint16 *)(areap)));
+    return (SDL_SwapBE16(*(Uint16 *)(areap)));
 }
 
 Uint32 SDLNet_Read32(void *areap)
 {
-	return (SDL_SwapBE32(*(Uint32 *)(areap)));
+    return (SDL_SwapBE32(*(Uint32 *)(areap)));
 }
 
 #endif /* !defined(WITHOUT_SDL) && !SDL_DATA_ALIGNED */
