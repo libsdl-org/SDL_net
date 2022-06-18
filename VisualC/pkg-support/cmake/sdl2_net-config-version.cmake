f09c579b22352fae126d4dbb9dcb6e3bbe4d60b4
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-06-18 13:06:18 +0200

    cmake: fix location of include dir for VC sdk

--- VisualC/pkg-support/cmake/sdl2_net-config-version.cmake
+++ VisualC/pkg-support/cmake/sdl2_net-config-version.cmake
@@ -8,7 +8,7 @@ if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/../include/SDL_net.h")
     return()
 endif()
 
-file(READ "${CMAKE_CURRENT_LIST_DIR}/../SDL_net.h" _sdl_net_h)
+file(READ "${CMAKE_CURRENT_LIST_DIR}/../include/SDL_net.h" _sdl_net_h)
 string(REGEX MATCH "#define[ \t]+SDL_NET_MAJOR_VERSION[ \t]+([0-9]+)" _sdl_major_re "${_sdl_net_h}")
 set(_sdl_major "${CMAKE_MATCH_1}")
 string(REGEX MATCH "#define[ \t]+SDL_NET_MINOR_VERSION[ \t]+([0-9]+)" _sdl_minor_re "${_sdl_net_h}")
