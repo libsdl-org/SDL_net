a3276bfaa89dacc21972c47db0bf02e986ef49cc
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-09-10 15:44:02 +0200

    Also detect UWP as windows

--- SDLnetUDP.c
+++ SDLnetUDP.c
@@ -22,7 +22,7 @@
 #include "SDLnetsys.h"
 #include "SDL_net.h"
 
-#if defined(__WIN32__) || defined(__OS2__)
+#if defined(_WIN32) || defined(__OS2__)
 #define srandom srand
 #define random  rand
 #endif
