641475e9a3df93b5396d23f502afec6d6a4d0a6c
ElpisActual <146009440+ElpisActual@users.noreply.github.com>
2025-04-21 17:51:01 +1000

    Reenable building with MSVC6 (#128)

--- SDLnetsys.h
+++ SDLnetsys.h
@@ -73,6 +73,10 @@ typedef int socklen_t;
 typedef Uint32 socklen_t;
 #endif
 
+#if defined(__USE_W32_SOCKETS) && !defined(IP_MSFILTER_SIZE)
+typedef int socklen_t;
+#endif
+
 /* System-dependent definitions */
 #ifndef __USE_W32_SOCKETS
 #ifdef __OS2__
